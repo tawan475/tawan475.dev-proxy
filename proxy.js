61112bd93a70fb3f6576591b52d606a15ad5928f
tawan475 <43491902+tawan475@users.noreply.github.com>
2022-07-08 06:12:03 +0700

    update errorHandler

--- proxy.js
+++ proxy.js
@@ -35,8 +35,7 @@ app.use(function (req, res, next) {
     next(createError(404));
 });
 
-// error handler
-app.use(function (err, req, res, next) {
+function errorHandler(err, req, res, next) {
     if (!err) return; // no error
     // render the error page
 	let errorID = uuidv4();
@@ -52,7 +51,10 @@ app.use(function (err, req, res, next) {
 	if (!err.status || err.status == 500) err.message = 'Internal Server Error';
 
     res.status(err.status || 500).json({ status: err.status || 500, errorID: errorID, message: err.message || 'Internal Server Error' });
-});
+}
+
+// error handler
+app.use(errorHandler);
 
 const options = process.env.NODE_ENV === "production" ? {
     key: fs.readFileSync('/etc/letsencrypt/live/tawan475.dev-0001/privkey.pem'),
