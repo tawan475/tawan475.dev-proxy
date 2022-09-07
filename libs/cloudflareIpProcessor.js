1a04bc87c03a5582be22d5134a7e2078aeb568f2
tawan475 <43491902+tawan475@users.noreply.github.com>
2022-09-07 23:05:04 +0700

    update cloudflareIpProcessor

--- libs/cloudflareIpProcessor.js
+++ libs/cloudflareIpProcessor.js
@@ -3,9 +3,12 @@ const cloudflareIP = require('@tawan475/cloudflareip');
 const cfIP = new cloudflareIP();
 
 module.exports = (req, res, next) => {
-    if (req.headers['cf-connecting-ip'] && cfIP.isCloudflareIP(req)) {
+    let forwardedFor = req.headers['x-forwarded-for'].split(',');
+    req.headers['cloudflare-ip'] = forwardedFor[1].trim();
+
+    if (req.headers['cf-connecting-ip'] && cfIP.isCloudflareIP( req.headers['cloudflare-ip'] )) {
         let realIP = ipaddr.process(req.headers['cf-connecting-ip']);
-        let cloudflareip = ipaddr.process(req.socket.remoteAddress);
+        let cloudflareip = ipaddr.process(req.headers['cloudflare-ip']);
 
         req.headers['from-cloudflare'] = true;
         req.headers['cloudflare-ip'] = cloudflareip;
@@ -18,10 +21,9 @@ module.exports = (req, res, next) => {
 
         req.cloudflare = true;
     } else {
-        let ip = ipaddr.process(req.socket.remoteAddress);
+        let ip = ipaddr.process(forwardedFor[0].trim());
         req.trustedip = ip.toString();
         req.trustedipver = ip.kind();
     }
-
     next();
 }
